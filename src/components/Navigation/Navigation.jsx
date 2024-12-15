1416b88b395fe88ca48985cd5890dcb8b5d3c9cb
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-12-15 01:58:15 -0700

    Add initial navigation bar component and basic styling

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import './Navigation.css'
+import React from "react";
+import "./Navigation.css";
 
 const Navigation = () => {
   return (
-    <nav className='nav'>
-        <div className="logo">NewsExplorer</div>
+    <nav className="nav">
+      <div className="nav__logo">NewsExplorer</div>
+      <div className="nav__links">
         <button className="nav__link">Home</button>
-        <button className="nav__link-pill-btn">Sign Up</button>
-    </nav>>
-  )
-}
+        <button className="nav__link-pill-btn">Sign in</button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
