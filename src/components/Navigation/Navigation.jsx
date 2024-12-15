44495b9712e02140ac2f22ff37111d4c51e16588
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-12-15 00:51:58 -0700

    center items on page

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navigation.css'
+
+const Navigation = () => {
+  return (
+    <nav className='nav'>
+        <div className="logo">NewsExplorer</div>
+        <button className="nav__link">Home</button>
+        <button className="nav__link-pill-btn">Sign Up</button>
+    </nav>>
+  )
+}
+
+export default Navigation
