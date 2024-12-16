a1473c29ec7e044370fced8ccd0b20ddd6fabc9d
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-12-15 21:50:13 -0700

    wip: SearchForm in progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Navigation from "../Navigation/Navigation";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header">
-      <h1 className="header__title">What's going on in the world?</h1>
+      <Navigation />
+      <h1 className="header__title">
+        What's going on in
+        <span className="header__title-second-line">the world?</span>
+      </h1>
       <p className="header__sub-title">
         Find the latest news on any topic and save them in your personal
         account.
