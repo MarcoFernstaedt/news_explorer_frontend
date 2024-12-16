b0565e4471125a0ff7673ec76b4aed36a636d455
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-12-16 01:21:10 -0700

    create: main section displaying news cards

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import NewsCard from "../NewsCard/NewsCard";
 import "./Main.css";
 
 const Main = () => {
   return (
     <main className="main">
       <h3 className="main__header">Search results</h3>
-      <ul className="main__card-wrap"></ul>
+      <ul className="main__card-wrap">
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+      </ul>
+      <button className="main__button">Show more</button>
     </main>
   );
 };
