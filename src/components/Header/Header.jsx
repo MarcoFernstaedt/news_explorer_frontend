fa53d599717f5c536d9517a989dc26d0bde0ece7
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-12-15 23:26:55 -0700

    create: header markup and style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import Navigation from "../Navigation/Navigation";
+import SearchForm from "../SearchForm/SearchForm";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header">
       <Navigation />
-      <h1 className="header__title">
-        What's going on in
-        <span className="header__title-second-line">the world?</span>
-      </h1>
-      <p className="header__sub-title">
-        Find the latest news on any topic and save them in your personal
-        account.
-      </p>
+      <div className="header__hero">
+        <h1 className="header__title">
+          What&apos;s going on in
+          <span className="header__title-second-line">the world?</span>
+        </h1>
+        <p className="header__sub-title">
+          Find the latest news on any topic and save them in your personal
+          account.
+        </p>
+      </div>
+      <SearchForm />
     </div>
   );
 };
