37f280a9be184b0c13ecfe55fae25ffc3bc57623
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-12-16 00:03:27 -0700

    wip: news cards in progress

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <main className="main">
+      <h3 className="main__header">Search results</h3>
+      <ul className="main__card-wrap"></ul>
+    </main>
+  );
+};
+
+export default Main;
