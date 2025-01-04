d3c2ee719ce3f9be7e074945ac4b4a5ac43c25f5
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-01-03 20:08:49 -0700

    wip: secondary modal button

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -2,19 +2,37 @@ import React from "react";
 import { useEscape } from "../../hooks/useEscape";
 import "./ModalWithForm.css";
 
-const ModalWithForm = ({ children, title, buttonText, onClose, onSubmit }) => {
+const ModalWithForm = ({
+  children,
+  title,
+  buttonText,
+  secondaryBtnText,
+  onClose,
+  onSubmit,
+  onSecondaryBtnSubmit,
+}) => {
   useEscape(onClose);
 
   return (
     <div className="modal">
       <div className="modal__container">
-        <button className="modal__close" onClose={onClose}></button>
+        <button className="modal__close" onClick={onClose}></button>
         <h3 className="modal__title">{title}</h3>
         <form action="" className="modal__form" onSubmit={onSubmit}>
           {children}
-          <button type="submit" className="modal__submit">
-            {buttonText}
-          </button>
+          {!secondaryBtnText ? (
+            <button type="submit" className="modal__submit">
+              {buttonText}
+            </button>
+          ) : (
+            <button
+              type="button"
+              onClick={onSecondaryBtnSubmit}
+              className="modal__secondaryBtn"
+            >
+              {secondaryBtnText}
+            </button>
+          )}
         </form>
       </div>
     </div>
