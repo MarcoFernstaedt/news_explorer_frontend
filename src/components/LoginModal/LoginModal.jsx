c19cee8a6bfcc5c601f5bc31f7cdea74a8a36e4c
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-01-08 22:49:03 -0700

    implament useForm hook

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
-const LoginModal = ({ title, buttonText, secondaryBtnText, onClose, onSubmit }) => {
+const LoginModal = ({
+  title,
+  buttonText,
+  secondaryBtnText,
+  onClose,
+  onSubmit,
+}) => {
+  const { values, handleChange } = useForm({});
   return (
     <ModalWithForm
       title={title}
@@ -10,23 +17,31 @@ const LoginModal = ({ title, buttonText, secondaryBtnText, onClose, onSubmit }) 
       onClose={onClose}
       onSubmit={onSubmit}
     >
-      <label htmlFor="email" className="modal__label">Email</label>
+      <label htmlFor="email" className="modal__label">
+        Email
+      </label>
       <input
         className="modal__input"
         type="email"
         name="email"
         id="email"
+        value={values.email || ""}
+        onChange={handleChange}
         minLength="2"
         maxLength="100"
         placeholder="Enter email"
       />
 
-      <label htmlFor="password" className="modal__label">Password</label>
+      <label htmlFor="password" className="modal__label">
+        Password
+      </label>
       <input
         className="modal__input"
         type="password"
         name="password"
-        id='password'
+        id="password"
+        value={values.password || ""}
+        onChange={handleChange}
         minLength="2"
         maxLength="100"
         placeholder="Enter password"
