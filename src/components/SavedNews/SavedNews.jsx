f901872fecf40240d9134e364800618284a086ac
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-01-20 13:49:33 -0700

    wip: markup for saved-news page

---
+++ src/components/SavedNews/SavedNews.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navigation from "../Navigation/Navigation";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+const SavedNews = () => {
+  return (
+    <div className="saved-news">
+      <Navigation />
+      <Header />
+      {/* <NewsCardSection /> */}
+      <Footer />
+    </div>
+  );
+};
+
+export default SavedNews;
