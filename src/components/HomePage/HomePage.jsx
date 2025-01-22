b2ca942c894400d8e15b262af5ced4c9b3f15ff1
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-01-22 13:47:13 -0700

    create homepage componetn to render main and about section.

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Main from "../Main/Main";
+import About from "../About/About";
+
+const HomePage = () => {
+  return (
+    <>
+      <Main />
+      <About />
+    </>
+  );
+};
+
+export default HomePage;
