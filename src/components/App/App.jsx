7cfa31ae5b1b00ede6ab84f94bd2f0d911b40683
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-01-22 13:48:54 -0700

    implamnet context provider passed values and created function to handle modal open

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,30 @@
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import About from "../About/About";
+import NewsContext from "../../context/newsContext.jsx";
 import Footer from "../Footer/Footer";
 import LoginModal from "../LoginModal/LoginModal";
 import RegisterModal from "../RegisterModal/RegisterModal";
 import "./App.css";
 import { useState } from "react";
-import { Route, Switch } from "react-router-dom";
+// import { Route, Switch } from "react-router-dom";
 import getNews from "../../utils/newsApi.jsx";
 
 const App = () => {
   const [activeModal, setActiveModal] = useState("open");
   const [newsArticles, setNewsArticles] = useState([]);
 
-  const handleOpenModal = () => {
-    setActiveModal("open");
+  const handleSearch = (keyword) => {
+    // search send api request.
+    setNewsArticles([keyword]);
+  };
+
+  const handleOpenLoginModal = () => {
+    setActiveModal("login");
+  };
+
+  const handleOpenRegisterModal = () => {
+    setActiveModal("register");
   };
 
   const handleCloseModal = () => {
@@ -23,33 +33,30 @@ const App = () => {
 
   return (
     <div className="app">
-      <div className="page">
-        {/* <Switch> */}
-          {/* <Route exact path="/"> */}
-            <Header />
-            {/* {newsArticles.length > 0 && <Main />} */}
-            <Main />
-            <About />
-          {/* </Route> */}
-        {/* </Switch> */}
-        <Footer />
-      </div>
-      {activeModal === "login" && (
-        <LoginModal
-          title="Sign in"
-          buttonText="Sign In"
-          secondaryBtnText="Sign up"
-          onClose={handleCloseModal}
-        />
-      )}
-      {activeModal === "register" && (
-        <RegisterModal
-          title="Sign in"
-          buttonText="Sign up"
-          secondaryBtnText="Login in"
-          onClose={handleCloseModal}
-        />
-      )}
+      <NewsContext.Provider value={{ newsArticles, handleSearch }}>
+        <div className="page">
+          <Header />
+          <Main />
+          <About />
+          <Footer />
+        </div>
+        {activeModal === "login" && (
+          <LoginModal
+            title="Sign in"
+            buttonText="Sign In"
+            secondaryBtnText="Sign up"
+            onClose={handleCloseModal}
+          />
+        )}
+        {activeModal === "register" && (
+          <RegisterModal
+            title="Sign in"
+            buttonText="Sign up"
+            secondaryBtnText="Login in"
+            onClose={handleCloseModal}
+          />
+        )}
+      </NewsContext.Provider>
     </div>
   );
 };
