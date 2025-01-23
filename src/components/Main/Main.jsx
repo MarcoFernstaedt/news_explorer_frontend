94613c0961f76bf58dacb275bb3c9b2791071f9d
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-01-23 16:17:03 -0700

    implament react context gather newsArticles to display

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import NewsCard from "../NewsCard/NewsCard";
+import { NewsContext } from "../../context/newsContext";
+import { useContext } from "react";
 import About from "../About/About";
 import "./Main.css";
 
 const Main = ({ newsCardData }) => {
+  const { newsArticles } = useContext(NewsContext);
+
   return (
     <main className="main">
       <h3 className="main__header">Search results</h3>
       <ul className="main__card-wrap">
+        <NewsCard />
         <NewsCard />
         <NewsCard />
         <NewsCard />
