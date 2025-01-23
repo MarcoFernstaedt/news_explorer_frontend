5068de5fdcac85d889a72282ab3f275090e33910
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-01-23 16:16:07 -0700

    implament context api for handleSearch function on submit/search

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,19 +1,38 @@
 import React from "react";
+import { NewsContext } from "../../context/newsContext";
+import { useForm } from "../../hooks/useForm";
+import { useContext } from "react";
 import "./SearchForm.css";
 
 const SearchForm = () => {
+  const { handleSearch } = useContext(NewsContext);
+  const { values, handleChange } = useForm({});
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    handleSearch(values);
+  };
+
   return (
     <form className="search-form">
       <input
         className="search-form__input"
         type="text"
         placeholder="Enter topic"
+        name="search"
+        id="search"
+        value={values.search || ""}
+        onChange={handleChange}
         aria-label="Search topic"
         minLength="2"
         maxLength="30"
         required
       />
-      <button className="search-form__btn" type="submit">
+      <button
+        onSubmit={handleSearch}
+        className="search-form__btn"
+        type="submit"
+      >
         Search
       </button>
     </form>
