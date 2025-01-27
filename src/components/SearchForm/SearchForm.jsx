f4be4fc56c2278e7c7244927ebf13475fb5c18af
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-01-27 12:08:43 -0700

    wip: getNews not found

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-// import { NewsContext } from "../../context/NewsContext.jsx";
+import NewsContext from "../../context/NewsContext.jsx";
 import { useForm } from "../../hooks/useForm";
 import { useContext } from "react";
 import "./SearchForm.css";
 
 const SearchForm = () => {
-  // const { handleSearch } = useContext(NewsContext);
+  const { handleSearch } = useContext(NewsContext);
   const { values, handleChange } = useForm({});
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     handleSearch(values);
   };
+
+  // console.log(articleArray)
 
   return (
     <form className="search-form">
