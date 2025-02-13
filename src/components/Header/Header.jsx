a5a081d0cc70a913629efd94a4c96c75d0df0387
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-02-13 14:39:35 -0700

    implament location based styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,25 +2,43 @@ import React from "react";
 import Navigation from "../Navigation/Navigation";
 import SearchForm from "../SearchForm/SearchForm";
 import "./Header.css";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ handleOpenLoginModal, handleOpenRegisterModal }) => {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
-    <div className="header">
+    <div className={isHome ? "header header_home" : "header_saved-news"}>
       <Navigation
         handleOpenLoginModal={handleOpenLoginModal}
         handleOpenRegisterModal={handleOpenRegisterModal}
       />
-      <div className="header__hero">
-        <h1 className="header__title">
-          What&apos;s going on in
-          <span className="header__title-second-line">the world?</span>
+      <div className={isHome ? "header__hero" : "header__hero_saved-news"}>
+        <h1
+          className={
+            isHome
+              ? "header__title"
+              : "header__title_saved-news"
+          }
+        >
+          {isHome ? "What's going on in" : "Marco you have 5 Saved"}
+          <span
+            className={
+              isHome
+                ? "header__title-second-line"
+                : "header__title-second-line_saved-news"
+            }
+          >
+            {isHome ? "the world?" : "Articles"}
+          </span>
         </h1>
         <p className="header__sub-title">
           Find the latest news on any topic and save them in your personal
           account.
         </p>
       </div>
-      <SearchForm />
+      {isHome && <SearchForm />}
     </div>
   );
 };
