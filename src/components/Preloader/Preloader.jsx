0f90d01b54269acd28066eef0ff31aad1a91f537
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-02-18 14:19:23 -0700

    Preloader animation and markup with error message

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Preloader = () => {
-  return <div className="circle-preloader">Preloader</div>;
+  return (
+    <div className="preloader">
+      <div className="circle-preloader">Preloader</div>
+      <h1 className="preloader__text" >Searching for news...</h1>
+    </div>
+  )
 };
 
 export default Preloader;
