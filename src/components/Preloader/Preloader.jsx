7d8cc014a55512df17349361f6f2b1accb44e232
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-02-20 15:00:39 -0700

    implament dynamic message and styling to stop spin animation

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
 import './preloader.css';
 
 const Preloader = () => {
+  const { isLoading } = useOutletContext();
+  
   return (
     <div className="preloader">
-      <div className="circle-preloader"></div>
-      <h1 className="preloader__text" >Searching for news...</h1>
+      <div className={isLoading ? 'circle-preloader' : 'circle-preloader circle-preloader_stop-animation'}></div>
+      <h2 className="preloader__text" >{isLoading ? 'Searching for news...' : 'Sorry but nothing matched your search terms'}</h2>
     </div>
   )
 };
