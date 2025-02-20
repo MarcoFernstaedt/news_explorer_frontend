c5310e0d0bd111398490ea9d857c727c16a97494
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-02-20 12:33:01 -0700

    wip: preloader styling

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Preloader = () => {
   return (
     <div className="preloader">
-      <div className="circle-preloader">Preloader</div>
+      <div className="circle-preloader"></div>
       <h1 className="preloader__text" >Searching for news...</h1>
     </div>
   )
