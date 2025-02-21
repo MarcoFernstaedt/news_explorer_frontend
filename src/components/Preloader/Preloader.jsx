6538bf85ece6b706e3fbe3e1c8bb0f6e83eafef1
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-02-20 17:57:05 -0700

    wrap second line of preloader__text in span tag

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { useOutletContext } from "react-router-dom";
-import './preloader.css';
+import "./preloader.css";
 
 const Preloader = () => {
   const { isLoading } = useOutletContext();
-  
+
   return (
     <div className="preloader">
-      <div className={isLoading ? 'circle-preloader' : 'circle-preloader circle-preloader_stop-animation'}></div>
-      <h2 className="preloader__text" >{isLoading ? 'Searching for news...' : 'Sorry but nothing matched your search terms'}</h2>
+      <div
+        className={
+          isLoading
+            ? "circle-preloader"
+            : "circle-preloader circle-preloader_stop-animation"
+        }
+      ></div>
+      <h2 className="preloader__text">
+        {isLoading ? (
+          "Searching for news..."
+        ) : (
+          <>
+            Sorry but nothing matched
+            <span className="preloader__text-second-line">
+              your search terms
+            </span>
+          </>
+        )}
+      </h2>
     </div>
-  )
+  );
 };
 
 export default Preloader;
