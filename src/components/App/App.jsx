2117cd55582c02b1ec0cf0fb525ad0f6ae488ad9
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-02-28 17:48:04 -0700

    pass isLoading through outlet context

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -86,6 +86,7 @@ const App = () => {
               visibleArticles,
               handleCardRender,
               isLoading,
+              isLoggedIn,
               hasSearched,
             }}
           />
